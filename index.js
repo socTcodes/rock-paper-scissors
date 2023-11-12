78f5f69f335fa0e746008608ce5a29062c356c13
SOCt33 <markshchukin13@gmail.com>
2023-11-12 14:37:53 -0800

    created a function to generate a random computer choice of rock, paper, or scissors.

---
+++ index.js
@@ -0,0 +1,13 @@
+function getComputerChoice () {
+    const randomNumber = Math.floor(Math.random() * 3);
+
+    if (randomNumber === 0) {
+        return "rock";
+    }   else if (randomNumber === 1) {
+        return "paper";
+    }   else {
+        return "scrissors";
+    }
+}
+
+console.log(getComputerChoice());
