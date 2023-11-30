e5d499243b0e9822e3e06def669fdd7cbaaf56db
SOCt33 <markshchukin13@gmail.com>
2023-11-30 12:58:51 -0800

    ui added

--- index.js
+++ index.js
@@ -1,25 +1,14 @@
-/*
-Create a function named getComputerChoice
-getComputerChoice should return rock, paper, or scissors
-Write a function that plays one round, it should have two parameters. playerSelection and computerSelection
-It should also return a string that declares a winner 
-Make playerSelection parameter input case in-sensistive
-Create a new function game() and add the previous made function into it that creates a 5 round game, keeps score, and declares a winner
-Call your playRound() function 5 times
-Use console.log to display the results of each round and the winner
-Use prompt to get input from user
-*/
 let playerScore = 0;
 let computerScore = 0;
 
-function getComputerChoice () {
+function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3);
 
     if (randomNumber === 0) {
         return "rock";
-    }   else if (randomNumber === 1) {
+    } else if (randomNumber === 1) {
         return "paper";
-    }   else {
+    } else {
         return "scissors";
     }
 }
@@ -28,39 +17,72 @@ console.log(getComputerChoice());
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        return "It's a tie!";
-    }   else if (
+        return "It's a tie! ";
+    } else if (
         (playerSelection === "rock" && computerSelection === "scissors") ||
         (playerSelection === "scissors" && computerSelection === "paper") ||
-        (playerSelection === "paper" && computerSelection === "rock") 
-    )   {
-        playerScore++
+        (playerSelection === "paper" && computerSelection === "rock")
+    ) {
         return `You win! ${playerSelection} beats ${computerSelection}!`;
-    }   else {
-        computerScore++
-        return `You lose! ${playerSelection} beats ${computerSelection}!`;
-    }
-}
-
-function game() {
-    while (playerScore < 5 && computerScore < 5) {
-        let userInput = prompt("rock, paper, or scissors?");
-        userInput = userInput.toLowerCase();
-
-        if (["rock", "paper", "scissors"].includes(userInput)) {
-            let computerChoice = getComputerChoice();
-            console.log(playRound(userInput, computerChoice));
-            console.log(`Player Score: ${playerScore} - Computer Score: ${computerScore}`);
-        } else {
-            console.log("Invalid choice. Please enter rock, paper, or scissors.");
-        }
-    }
-
-    if (playerScore === 5) {
-        alert("You won the game!");
     } else {
-        alert("Computer won the game!");
+        return `You lose! ${computerSelection} beats ${playerSelection}!`;
     }
 }
 
-game();
+function game(userChoice) {
+    if (["rock", "paper", "scissors"].includes(userChoice)) {
+        let computerChoice = getComputerChoice();
+
+        let result = playRound(userChoice, computerChoice);
+
+        if (result.includes("win")) {
+            playerScore++;
+        } else if (result.includes("lose")) {
+            computerScore++;
+        }
+        
+        if (playerScore === 5 || computerScore === 5) {
+            disableButtons();
+        }
+
+        results.textContent = result;
+        results.textContent += ` Player score: ${playerScore} - Computer score: ${computerScore}`;
+
+        if (playerScore === 5) {
+            results.textContent = "You won the game!";
+            disableButtons();
+        } else if (computerScore === 5) {
+            results.textContent = "Computer won the game!";
+            disableButtons();
+        }
+    } else {
+        results.textContent = "Invalid choice. Please enter rock, paper, or scissors.";
+    }
+} 
+
+function disableButtons() {
+    rock.removeEventListener('click', rockClickHandler);
+    paper.removeEventListener('click', paperClickHandler);
+    scissors.removeEventListener('click', scissorsClickHandler);
+}
+
+function rockClickHandler() {
+    game('rock');
+}
+
+function paperClickHandler() {
+    game('paper');
+}
+
+function scissorsClickHandler() {
+    game('scissors');
+}
+
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
+const results = document.getElementById('results');
+
+rock.addEventListener('click', rockClickHandler);
+paper.addEventListener('click', paperClickHandler);
+scissors.addEventListener('click', scissorsClickHandler);
